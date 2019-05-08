20de90f521df998767db59ab66105a550c663ff8
刘伟坤 <1834183435@qq.com>
2019-05-08 18:32:41 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
-// export const BASE_API = "https://rapapi.renqilai.com/app/mock/39";
+ export const BASE_API = "https://rapapi.renqilai.com/app/mock/39";
 
 export const client_id = 'test'
 export const client_secret = "test"
 // export const BASE_API = "http://192.168.0.222:8080"
-export const BASE_API = "https://quxiaapi.renqilai.com"
 
 export const pageSize = 20
